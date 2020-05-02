560c324bfc8ed22ddb0e966a42219c4783dd7eda
Edgar Bekier <edgar.bekier@devinci.fr>
2020-05-02 12:13:16 +0200

    misajour

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,10 @@
 Rails.application.configure do
+
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'educaia.herokuapp.com', :protocol => 'https'}
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
