8538319a26300778c85a2b6fa9f9b488ff132107
Edgar Bekier <sieegate@gmail.com>
2020-12-19 19:49:42 +0100

    testlast

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'educaia.herokuapp.com', :protocol => 'https'}
+  config.action_mailer.default_url_options = { :host => 'https://educaia.herokuapp.com/', :protocol => 'https'}
 
   # Settings specified here will take precedence over those in config/application.rb.
 
